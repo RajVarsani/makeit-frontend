14b9e8b40612880472bd383edcf4dd817f09eec7
Vaishnavi <imt_2020109@iiitm.ac.in>
2022-02-23 22:59:37 +0530

    Setup redux

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
 
-import { useSelector } from "react-redux";
-
 import styles from "./Home.module.css";
 
 const Home = () => {
-  const exampleState = useSelector((state) => state.example.exampleState);
-
   return <div className={styles.Wrapper}></div>;
 };
 
